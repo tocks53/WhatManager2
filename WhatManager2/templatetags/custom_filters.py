67570a5ff1109b968d2f9057d4854bac33fbfdce
tocks53 <tocks0000@gmail.com>
2016-12-10 15:52:48 +0100

    Change apollo.rip

--- WhatManager2/templatetags/custom_filters.py
+++ WhatManager2/templatetags/custom_filters.py
@@ -83,7 +83,7 @@ def filter_release_type_name(value):
 
 @register.filter
 def what_cd_torrent_link(value):
-    return u'https://passtheheadphones.me/torrents.php?torrentid={0}'.format(value)
+    return u'https://apollo.rip/torrents.php?torrentid={0}'.format(value)
 
 
 @register.filter
